35832c0528a8202c7715bd95c43c24229920bb56
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2022-09-19 11:39:39 +0000

    Added basicMath.cpp

---
+++ basicMath.cpp
@@ -0,0 +1,17 @@
+// COpyright 2022 Peter Sobowale
+// Created by: Peter Sobowale
+// Created on: Sept. 18, 2022
+// This program does basic math.
+
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "9 + 2 = " << 9 + 2 << std::endl;
+    std::cout << "7 - 3 = " << 7 - 3 << std::endl;
+    std::cout << "10 / 2 = " << 10 / 2 << std::endl;
+    std::cout << "8 - 4 / 2 = " << 8 - 4 / 2 << std::endl;
+    std::cout << "2^3 = " << pow(2, 3) << std::endl;
+    std::cout << "1 / 3 = " << float(1) / float(3) << std::endl;
+}
