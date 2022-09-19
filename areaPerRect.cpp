abb96504e0a9e58343e66c3925fb357712a999ae
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2022-09-19 14:49:53 +0000

    Added areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,14 @@
+// Copyright 2022 Peter Sobowale
+// Created by: Peter Sobowale
+// Created on: Sept 18, 2022
+// This program calculates and displays the area and
+// perimeter of a rectangle with dimensions 5cm X 3cm.
+#include <iostream>
+
+int main() {
+    std::cout << "For a rectangle that has dimensions\n";
+    std::cout << "length 5cm and width 3cm:\n";
+    std::cout << "\n";
+    std::cout << "Area = " << 5 * 3 << " cm^2\n";
+    std::cout << "Perimeter = " << 2 * (5 + 3) << " cm\n";
+}
